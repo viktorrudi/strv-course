837fa215d17f8246c1e5960906fbdf532e26a314
Viktor Rudi <vru@westcom.local>
2019-04-03 20:39:03 +0200

    week3

---
+++ homework/week3/index.js
@@ -0,0 +1,18 @@
+// 'use strict';
+
+// const net = require('net')
+
+// net.createServer(socket => {
+//   console.log('connected')
+
+//   socket.on('data', data =>{
+//     console.log(`Got data`);
+//     process.stdout.write(data)
+
+//     socket.write('input received')
+//   })
+
+//   socket.on('close',()=>{
+//     console.log('disconnected');
+//   })
+// }).listen(3000)
