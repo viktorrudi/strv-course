8c7de73ec6df5a0d06e1e5b07a6ef1b722ca935e
Viktor Rudi <vru@westcom.local>
2019-04-04 16:58:52 +0200

    update

--- homework/week3/app.js
+++ homework/week3/app.js
@@ -1,16 +1,29 @@
 const Koa = require('koa')
 const KoaRouter = require('koa-router')
+const path = require('path')
+// const render = require('koa-ejs')
 
 const app = new Koa();
 const router = new KoaRouter();
+
+// ?
+// render(app, {
+//   root: path.join(__dirname, 'views'),
+//   layout: 'layout',
+//   viewExt: 'html',
+//   cache: false,
+//   debug: false
+// })
+
 
 // Simple middleware creation
 app.use(async ctx => {
   ctx.body = 'Hello world'
 })
 
-router.get('/path', ctx => {
-  ctx.body = 'path here'
+// Not working?
+router.get('/test', ctx => {
+  ctx.body = 'test here'
 })
 
 // Router middleware
